757304778f2e0e90609d9e844f64fec4daea5209
marcelooliveira <mclricardo@gmail.com>
2025-04-26 09:38:13 -0300

    Update flow.py

--- flow.py
+++ flow.py
@@ -1,28 +1,12 @@
-# /// script
-# dependencies = ["prefect"]
-# ///
+from prefect import flow
 
-"""
-A simple flow that says hello.
-"""
-
-from prefect import flow, get_run_logger, tags
-
-
-# The name of the flow, `hello` is inferred from the function name by default
-# The arguments to the flow are type annotated and Prefect will validate them at runtime
-@flow
-def hello(name: str = "Marvin"):
-    get_run_logger().info(f"Hello, {name}!")
-
+@flow(log_prints=True)
+def hello():
+  print("Hello!")
 
 if __name__ == "__main__":
-    # Run the flow
-    hello()  # Output: "Hello, Marvin!"
-
-    # Run the flow with a different argument
-    hello("Arthur")  # Output: "Hello, Arthur!"
-
-    # Run the flow with a "local" tag
-    with tags("local"):
-        hello()
+    hello.deploy(
+        name="cicd-deployment",
+        work_pool_name="my-work-pool",
+        image="mclricardo/cicd_image:latest",
+    )
